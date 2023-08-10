8c2c5fad3e2653b18b27728f9c7ff50efea063b3
adityachintala <adityachintala27@gmail.com>
2023-08-10 14:47:45 +0530

    Removed unnecessary dummy Google auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Welcome from './components/Welcome';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+// import Welcome from './components/Welcome';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AddWord from './components/AddWord';
@@ -15,22 +15,34 @@ const PrivateRoute = ({ children, authed }) => {
 };
 
 const App = () => {
-	const [authed, setAuthed] = useState(false);
+	// const [authed, setAuthed] = useState(false);
 
 	return (
-		<GoogleOAuthProvider clientId="640525177814-26mmv0ss9dcnrem37g8qlc47ahqi8m0g.apps.googleusercontent.com">
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route exact path="/" element={<Welcome setAuthed={setAuthed} />} />
-					<Route path="/home" element={<PrivateRoute authed={authed}> <Home /> </PrivateRoute>} />
-					<Route path="/add-word" element={<PrivateRoute authed={authed}> <AddWord /> </PrivateRoute>} />
-					<Route path="/test" element={<PrivateRoute authed={authed}> <Test /> </PrivateRoute>} />
-					<Route path="/test/init-words" element={<PrivateRoute authed={authed}> <InitWords /> </PrivateRoute>} />
-					<Route path="/test/user-words" element={<PrivateRoute authed={authed}> <UserWords /> </PrivateRoute>} />
-				</Routes>
-			</Router>
-		</GoogleOAuthProvider>
+		// <GoogleOAuthProvider clientId="640525177814-26mmv0ss9dcnrem37g8qlc47ahqi8m0g.apps.googleusercontent.com">
+		// 	<Router>
+		// 		<Navbar />
+		// 		<Routes>
+		// 			<Route exact path="/" element={<Welcome setAuthed={setAuthed} />} />
+		// 			<Route path="/home" element={<PrivateRoute authed={authed}> <Home /> </PrivateRoute>} />
+		// 			<Route path="/add-word" element={<PrivateRoute authed={authed}> <AddWord /> </PrivateRoute>} />
+		// 			<Route path="/test" element={<PrivateRoute authed={authed}> <Test /> </PrivateRoute>} />
+		// 			<Route path="/test/init-words" element={<PrivateRoute authed={authed}> <InitWords /> </PrivateRoute>} />
+		// 			<Route path="/test/user-words" element={<PrivateRoute authed={authed}> <UserWords /> </PrivateRoute>} />
+		// 		</Routes>
+		// 	</Router>
+		// </GoogleOAuthProvider>
+
+
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/add-word" element={<AddWord />} />
+				<Route path="/test" element={<Test />} />
+				<Route path="/test/init-words" element={<InitWords />} />
+				<Route path="/test/user-words" element={<UserWords />} />
+			</Routes>
+		</Router>
 	);
 };
 
